fd6b650716751dc6560526e86767bbe236a21e53
pawel.gawin <pawel.gawin@PK4-8>
2019-03-21 09:44:04 +0100

    update

--- Cw_02/Program.cs
+++ Cw_02/Program.cs
@@ -13,14 +13,14 @@ namespace Cw_02
             Random rand = new Random();
             double[] tab1 = new double[30];
 
-            for (int ctr = 0; ctr < 30; ctr++)
+            for (int ctr = 0; ctr < tab1.Length; ctr++)
             {
                 tab1[ctr] = rand.Next(101);
                 Console.WriteLine(tab1[ctr]);
 
             }
             int random_number = rand.Next(31);
-            Console.WriteLine("Loswy index tablicy: \n"+tab1[random_number] +" Idnex: " + random_number);
+            Console.WriteLine("Loswy index tablicy: \n" + tab1[random_number] +" Idnex: " + random_number);
             
             Console.ReadKey();
         }
--- Cw_04/Program.cs
+++ Cw_04/Program.cs
@@ -11,11 +11,29 @@ namespace Cw_04
         static void Main(string[] args)
         {
             Random rand = new Random();
-            int number = rand.Next(101);
+            int a = rand.Next(101);
+            int b = rand.Next(101);
+            int c = rand.Next(101);
+            double delta;
 
-            Console.WriteLine(number + "\n");
+            delta = Math.Pow(b, 2) - 4 * a * c;
+            double x1, x2;
+            if(delta > 0)
+            {
+                x1 = (-b - Math.Sqrt(delta)) / (2 * a);
+                x2 = (-b + Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("Miejsca zerowe: " + x1 + " " + x2);
+            }
+            else if(delta < 0 )
+            {
+                x1 = -b / (2 * a);
+                Console.WriteLine("Miejsce zerowe: " + x1);
+            }
+            else
+            {
+                Console.WriteLine("funkcja nie posiada miejsc zerowych");
+            }
 
-            Console.WriteLine(Math.Sqrt(number));
 
             Console.ReadKey();
         }
