a0452543c579272b122cbac5a42eefacd08ca3f4
pawel.gawin <pawel.gawin@PK4-8>
2019-04-25 09:20:49 +0200

    added boarddraw function

--- Kolko_i_krzyzyk/Program.cs
+++ Kolko_i_krzyzyk/Program.cs
@@ -10,18 +10,48 @@ namespace Kolko_i_krzyzyk
     {
         static void Main(string[] args)
         {
-            // man code for tictactoe game
+            // mai n code for tictactoe game
             string playerName1 = "";
             string playerName2 = "";
             char playerSymbolX = 'x';
             char playerSymbolO = 'o';
-            char[,] board = new char[3, 3];
+            char[,] board = new char[3, 3] {
+                { '1', '2', '3' },
+                { '4', '5', '6' },
+                { '7', '8', '9' }
+            };
 
             Console.WriteLine("Enter name of player 1: ");
             playerName1 = Console.ReadLine();
             Console.WriteLine("Enter name of player 2: ");
             playerName2 = Console.ReadLine();
 
+            //A loop over players moves
+            bool gameOver = false;
+            while (!gameOver)
+            {
+                Console.Clear();
+                DrawBorad(board);
+
+                Console.ReadKey();
+            }
+
+            
+        }
+
+        static void DrawBorad(char[,] board)
+        {
+            int height = board.GetLength(0);
+            int width = board.GetLength(1);
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(board[i,j]+ " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
