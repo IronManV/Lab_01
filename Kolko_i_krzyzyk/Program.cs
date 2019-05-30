0234d9f39f912a83f6da06605be082aef6b767af
pawel.gawin <pawel.gawin@PK4-8>
2019-05-30 09:03:18 +0200

    Add game over check

--- Kolko_i_krzyzyk/Program.cs
+++ Kolko_i_krzyzyk/Program.cs
@@ -25,10 +25,12 @@ namespace Kolko_i_krzyzyk
                 { '7', '8', '9' }
             };
 
+            char[,] boardCopy = board.Clone() as char[,];
+
             //A loop over players moves
             bool gameOver = false;
             bool movePlayerA = true;
-            while (!gameOver)
+            for (int round = 0; round < board.Length; ++round)
             {
                 Console.Clear();
                 DrawBorad(board);
@@ -36,13 +38,13 @@ namespace Kolko_i_krzyzyk
                 if (movePlayerA)
                 {
                     Console.WriteLine("current player: " + gA.Name);
-                    gameOver = gA.MakeMove(board);
+                    gameOver = gA.MakeMove(board, boardCopy);
                     movePlayerA = false;
                 }
                 else
                 {
                     Console.WriteLine("current player: " + gB.Name);
-                    gameOver = gB.MakeMove(board);
+                    gameOver = gB.MakeMove(board, boardCopy);
                     movePlayerA = true;
                 }
                 Console.ReadKey();
@@ -68,28 +70,75 @@ namespace Kolko_i_krzyzyk
     }
     interface IMove
     {
-        bool MakeMove(char[,] board);
+        bool MakeMove(char[,] board, char[,] boardCopy);
     }
 
     abstract class Player
     {
         public string Name { get; set; }
         public char Symbol { get; set; }
+
+        public bool CheckIfGameOver(char[,] board)
+        {
+            int height = board.GetLength(0);
+            int width = board.GetLength(1);
+            if (width != height)
+                throw new Exception("The board is not a square!");
+
+            //check rows
+            for(int i = 0; i < height; i++)
+            {
+                int sumOfRow = 0;
+                for (int j = 0; j < width; j++)
+                {
+                    if (board[i, j] == Symbol)
+                        sumOfRow++;
+                }
+                if (sumOfRow == width)
+                    return true;
+            }
+            //check columns
+            for (int j = 0; j < width; j++)
+            {
+                int sumOfColumn = 0;
+                for (int i = 0; i < height; i++)
+                {
+                    if (board[i, j] == Symbol)
+                        sumOfColumn++;
+                }
+                if (sumOfColumn == height)
+                    return true;
+            }
+            //check diagonals
+            int sumOfDiagonalA = 0;
+            int sumOfDiagonalB = 0;
+            for(int k = 0;k < width; k++)
+            {
+                if (board[k, k] == Symbol)
+                    sumOfDiagonalA++;
+                if (board[k, width - 1 - k] == Symbol)
+                    sumOfDiagonalB++;
+            }
+            if (sumOfDiagonalA == width || sumOfDiagonalB == width)
+                return true;
+
+            return false;
+        }
     }
 
     class PlayerHuman : Player, IMove
     {
-        public bool MakeMove(char[,] board)
+        public bool MakeMove(char[,] board, char[,] boardCopy)
         {
-            return false; // to correct
+            return CheckIfGameOver(board); // to correct
         }
     }
 
     class PlayerComputer : Player, IMove
     {
-         public bool MakeMove(char[,] board)
+         public bool MakeMove(char[,] board, char[,] boardCopy)
         {
-            return false; //to correct
+            return CheckIfGameOver(board); //to correct
         }
     }
 }
